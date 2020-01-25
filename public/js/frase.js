30bf83b1194762da1860aeba983b517a0329e4a8
Matheus dos Santos <matheusmendes1@hotmail.com>
2020-01-25 10:17:53 -0300

    End Chapter 2 - Part 2

---
+++ public/js/frase.js
@@ -0,0 +1,14 @@
+$("#botao-frase").click(fraseAleatoria);
+
+function fraseAleatoria() {
+    $.get("http://localhost:3000/frases", trocaFraseAleatoria);
+}
+
+function trocaFraseAleatoria(data){
+    let frase = $('.frase');
+    let numAleatorio = Math.floor(Math.random() * data.length);
+     
+    frase.text(data[numAleatorio].texto);
+    atualizaTamanhoFrase();
+    atualizaTempoInicial(data[numAleatorio].tempo);
+}
