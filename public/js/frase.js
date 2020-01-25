e9ba81f29d2bef2fb2a86aa39e3123cb34c0fda2
Matheus dos Santos <matheusmendes1@hotmail.com>
2020-01-25 12:28:21 -0300

    End Chapter 4 - part 2

--- public/js/frase.js
+++ public/js/frase.js
@@ -1,7 +1,21 @@
-$("#botao-frase").click(fraseAleatoria);
+$('#botao-frase').click(fraseAleatoria);
+$('#botao-frase-id').click(buscaFrase);
 
 function fraseAleatoria() {
-    $.get("http://localhost:3000/frases", trocaFraseAleatoria);
+    
+    $('#spinner').toggle();
+
+    $.get('http://localhost:3000/frases', trocaFraseAleatoria)
+    .fail(() => {
+        $('#erro').toggle();
+
+        setTimeout(() => {
+            $('#erro').toggle();
+        }, 2000);
+    })
+    .always(() => {
+        $('#spinner').toggle();
+    });
 }
 
 function trocaFraseAleatoria(data){
@@ -11,4 +25,32 @@ function trocaFraseAleatoria(data){
     frase.text(data[numAleatorio].texto);
     atualizaTamanhoFrase();
     atualizaTempoInicial(data[numAleatorio].tempo);
+}
+
+function buscaFrase(){
+
+    $('#spinner').toggle();
+
+    let fraseId = $('#frase-id').val();
+    let dados = { id: fraseId }
+
+    $.get('http://localhost:3000/frases', dados, trocaFrase)
+    .fail(() => {
+        $('#erro').toggle();
+
+        setTimeout(() => {
+            $('#erro').toggle();
+        }, 2000);
+    })
+    .always(() => {
+        $('#spinner').toggle();
+    });
+}
+
+
+function trocaFrase(data){
+    let frase = $('.frase');
+    frase.text(data.texto);
+    atualizaTamanhoFrase();
+    atualizaTempoInicial(data.tempo);
 }
